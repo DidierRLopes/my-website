432aa82f2ff2751e6db2e36831e0ae1f8df9a4f0
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-11-04 17:43:08 -0700

    add articles

--- my-website/content/resume/articles.md
+++ my-website/content/resume/articles.md
@@ -3,4 +3,9 @@ title: Articles
 sidebar_position: 2
 ---
 
-# Articles
+import ResumeArticles from '@site/src/components/ResumeArticles.tsx'
+import HeadTitle from '@site/src/components/General/HeadTitle.tsx'
+
+<HeadTitle title="Articles" />
+
+<ResumeArticles />
---
+++ my-website/src/components/General/ArticlesList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface Article {
+  title: string;
+  conference: string;
+  link: string;
+}
+
+interface ArticleListProps {
+  articles: Article[];
+}
+
+export default function ArticlesList({ articles }: ArticleListProps) {
+  return (
+    <div className="mx-auto mt-8">
+      {articles.map((article) => (
+        <div className="container relative justify-center items-center mb-8 my-4 mx-2 border-[1px] p-2 rounded border-[#2e8555]">
+          <div>
+            <div className='justify-left items-start text-xs'>
+              {article.conference}
+            </div>
+            <h3 className="justify-center items-center text-base mb-2">
+              {article.title}
+            </h3>
+            <a
+              href={article.link}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="justify-center items-center text-base mb-2"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};

4c9e6a345fe2306f436a0507a9b0bdb30732e101
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2024-03-09 23:06:08 -0500

    carousel of images for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function Home() {
   return (
@@ -22,23 +24,51 @@ export default function Home() {
               OpenBB
             </a>
           </div>
-          <div className="flex items-center content-center mx-auto align-center justify-center flex-wrap mt-4 gap-8 mb-4"> 
-            <img
-              className="rounded-3xl max-h-[300px] mx-auto"
-              src="img/tattoo.webp"
-              alt="OpenBB Tattoo"
-            />
-            <img
-              className="rounded-3xl max-h-[300px] mx-auto"
-              src="img/me_timegpt.webp"
-              alt="Time-GPT event from Nixtla"
-            />
-            <img
-              className="rounded-3xl max-h-[300px] mx-auto"
-              src="img/bridge_sticker.webp"
-              alt="OpenBB bridget sticker"
-            />
-          </div>
+          {window.innerWidth <= 1024 ? (
+            <div className="flex max-w-[240px] items-center content-center mx-auto align-center justify-center flex-wrap mt-4 gap-8 mb-4"> 
+              <Carousel showThumbs={false} showStatus={false}>
+                <div>
+                  <img
+                    className="rounded-3xl max-h-[300px] mx-auto"
+                    src="img/me_timegpt.webp"
+                    alt="Time-GPT event from Nixtla"
+                  />
+                </div>
+                <div>
+                  <img
+                    className="rounded-3xl max-h-[300px] mx-auto"
+                    src="img/tattoo.webp"
+                    alt="OpenBB Tattoo"
+                  />
+                </div>
+                <div>
+                  <img
+                    className="rounded-3xl max-h-[300px] mx-auto"
+                    src="img/bridge_sticker.webp"
+                    alt="OpenBB bridget sticker"
+                  />
+                </div>
+              </Carousel>
+            </div>
+          ) : (
+            <div className="flex items-center content-center mx-auto align-center justify-center flex-wrap mt-4 gap-8 mb-4"> 
+              <img
+                className="rounded-3xl max-h-[300px] mx-auto"
+                src="img/tattoo.webp"
+                alt="OpenBB Tattoo"
+              />
+              <img
+                className="rounded-3xl max-h-[300px] mx-auto"
+                src="img/me_timegpt.webp"
+                alt="Time-GPT event from Nixtla"
+              />
+              <img
+                className="rounded-3xl max-h-[300px] mx-auto"
+                src="img/bridge_sticker.webp"
+                alt="OpenBB bridget sticker"
+              />
+            </div>
+          )}
         </div>
         <div className="mx-auto mt-16 flex max-w-[880px] flex-col px-3 text-center rounded-[14px]">
           <h1>
@@ -46,18 +76,31 @@ export default function Home() {
           </h1>
           <ul className='mt-4 text-left'>
             <p>🇨🇭 I was born in Switzerland, but all my family are Portuguese.</p>
+            <br/>
             <p>🇵🇹 Moved to Portugal when I was 8yo and did a BSc. in Electrical and Computer Engineering.</p>
+            <br/>
             <p>🇳🇱 Did a semester in TU Delft and learned about self-driving cars.</p>
+            <br/>
             <p>🇬🇧 Moved to London to do an MSc. in Control Systems at Imperial College London.</p>
+            <br/>
             <p>🚗 Excited about AI and self-driving cars, so I worked as a Sensor Fusion Engineer.</p>
+            <br/>
             <p>🎓 Wrote the code behind my math teacher's thesis on <a href="https://github.com/DidierRLopes/UnivariateTimeSeriesForecast" target="_blank" rel="noreferrer">Forecasting of Financial timeseries</a>.</p>
+            <br/>
             <p>😤 Performed my own investment research and got frustrated by how time-consuming it was.</p>
+            <br/>
             <p>🦠 During Covid my flight got cancelled over holidays. So started building my financial terminal.</p>
+            <br/>
             <p>🦍 I made Gamestonk Terminal open source and we went viral on Reddit and HackerNews.</p>
+            <br/>
             <p>📈 Raised $8.9M in a seed round to democratize investment research.</p>
+            <br/>
             <p>🦋 Announced OpenBB to the world in 2022 and rebranded the terminal.</p>
+            <br/>
             <p>🇺🇸 Took my wife and dogs to live in the Bay area, to be closer to tech founders and investors.</p>
+            <br/>
             <p>🗽 Moved with my wife and dogs again to be closer to customers in NYC.</p>
+            <br/>
           </ul>
           <div className="flex-none overflow-y-scroll rounded-sm mx-auto text-base mt-4">
             <p>

990f19999ba00747c08ddb42ec9eeea72f2e6030
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2024-07-20 17:49:29 -0400

    use book style from @realvjy - here https://codepen.io/realvjy/pen/KKEdXOa

--- src/components/General/BooksList.tsx
+++ src/components/General/BooksList.tsx
@@ -11,25 +11,19 @@ interface BooksListProps {
 }
 
 export default function BooksList({ books }: BooksListProps) {
-
   return (
     <div>
-      <div className="mx-auto mt-8 md:grid md:grid-cols-2">
+      <div className="book-wrapper">
         {books.map((book) => (
-          <div className='flex my-4 mx-2 border-[1px] p-2 gap-6 rounded border-[#0088CC]'>
-            <div className='w-[100px] flex justify-center items-center'>
-              <img
-                src={book.image}
-                alt={book.title}
-                style={{ width: '100px', objectFit: 'cover' }}
-              />
-            </div>
-            <div className="flex flex-col justify-center">
-              <div className="font-bold md:text-base" style={{ maxWidth: '200px', wordWrap: 'break-word', lineHeight: '1' }}>
-                {book.title}
-              </div>
-              <div className="text-xs mt-2">
-                {book.author}
+          <div className="book-items" key={book.title}>
+            <div className="main-book-wrap">
+              <div className="book-cover">
+                <div className="book-inside"></div>
+                <div className="book-image">
+                  <img src={book.image} alt={`${book.title} from ${book.author}`} />
+                  <div className="effect"></div>
+                  <div className="light"></div>
+                </div>
               </div>
             </div>
           </div>
@@ -37,4 +31,4 @@ export default function BooksList({ books }: BooksListProps) {
       </div>
     </div>
   );
-};
+}

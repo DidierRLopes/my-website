64b8e9554b5d93563b23dc95c46d1973f35a0ebf
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2024-09-15 16:55:29 -0400

    try to fix action

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,30 +1,44 @@
 import { useBlogPost } from '@docusaurus/theme-common/internal';
 import BlogPostItem from '@theme-original/BlogPostItem';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function BlogPostItemWrapper(props) {
   const { metadata, isBlogPostPage } = useBlogPost();
+  const isBrowser = useIsBrowser();
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (isBrowser) {
+      setTheme(document.documentElement.getAttribute('data-theme') !== 'dark' ? 'light' : 'dark');
+    }
+  }, [isBrowser]);
 
   return (
     <>
       <BlogPostItem {...props}>
         {props.children}
         {isBlogPostPage && (
-          <div>
-            <iframe
-              src={
-                document.documentElement.getAttribute('data-theme') !== 'dark'
-                  ? 'https://embeds.beehiiv.com/e1ef8c12-fc6d-4afa-8235-057b2e9bb6f3'
-                  : 'https://embeds.beehiiv.com/2c2719b8-abe1-4f8b-8427-fb9c2361f059'
-              }
-              data-test-id="beehiiv-embed"
-              width="100%"
-              height="200"
-              frameBorder="0"
-              scrolling="no"
-              title="Didier newsletter"
-            />
-          </div>
+          <BrowserOnly>
+            {() => (
+              <div>
+                <iframe
+                  src={
+                    theme === 'light'
+                      ? 'https://embeds.beehiiv.com/e1ef8c12-fc6d-4afa-8235-057b2e9bb6f3'
+                      : 'https://embeds.beehiiv.com/2c2719b8-abe1-4f8b-8427-fb9c2361f059'
+                  }
+                  data-test-id="beehiiv-embed"
+                  width="100%"
+                  height="200"
+                  frameBorder="0"
+                  scrolling="no"
+                  title="Didier newsletter"
+                />
+              </div>
+            )}
+          </BrowserOnly>
         )}
       </BlogPostItem>
     </>

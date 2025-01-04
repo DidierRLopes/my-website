48ce8756e1e68cc1409de369ce6985eed69b08ec
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2025-01-04 12:28:38 -0500

    remove react-vertical-timeline-component which is unused

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
 
 const journeyData = [
   {

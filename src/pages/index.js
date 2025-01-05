1c09ef0b6854a1954c441127e2ba76f5881ccbf0
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2025-01-04 23:23:25 -0500

    fix now

--- src/pages/index.js
+++ src/pages/index.js
@@ -230,14 +230,14 @@ export default function Home() {
                 <iframe
                   src={githubSrc}
                   title="GitHub Stats"
-                  className="!max-w-[300px] h-[200px] border-0"
+                  className="w-[300px] h-[200px] border-0"
                 />
               </div>
               <div className="flex items-center justify-center h-full pt-8">
                 <iframe
                   src="https://ssr-contributions-svg.vercel.app/_/DidierRLopes?chart=3dbar&format=svg&theme=blue"
                   title="GitHub Contributions"
-                  className="!max-w-[300px] h-[200px] border-0"
+                  className="w-[300px] h-[200px] border-0"
                 />
               </div>
             </Carousel>
